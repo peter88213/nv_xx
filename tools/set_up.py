8d3518ec85cf954f913e6a992961abd9a2d230e3
peter88213 <petert88213@gmail.com>
2024-08-18 16:35:51 +0200

    Update Copyright note.

--- tools/set_up.py
+++ tools/set_up.py
@@ -2,7 +2,7 @@
 
 - Generate the language specific '*.po' dictionaries for novelibre and its plugins.
 
-Copyright (c) 2023 Peter Triesberger
+Copyright (c) 2024 Peter Triesberger
 For further information see https://github.com/peter88213/novelibre
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
