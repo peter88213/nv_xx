0b15cb8e3e4f7282619e20f6ca0d9d98e45d95a4
peter88213 <petert88213@gmail.com>
2025-08-21 13:00:33 +0200

    Added a script for help page creation.

---
+++ tools/create_help_pages.py
@@ -0,0 +1,25 @@
+"""Create help pages to translate.
+
+Reads the ../docs/index.md file entries, and creates the listed pages.
+
+"""
+import os
+import re
+
+help_dir = '../docs'
+page_pattern = re.compile(r'\[(.*?)\]\((.*?)\)')
+
+with open(f'{help_dir}/index.md', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+page_data = page_pattern.findall(text)
+for title, file_name in page_data:
+    page_name, __ = os.path.splitext(file_name)
+    with open(f'{help_dir}/{file_name}', 'w', encoding='utf_8') as f:
+        f.write(
+            f'[home](index.md) > {title}'
+            '\n\n---\n\n'
+            f'# {title}\n\n'
+            '\n\n---\n\n'
+            f'[English](https://peter88213.github.io/nvhelp-en/{page_name}.html)'
+        )
