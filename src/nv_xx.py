0fd4a1964b478db49a31cd52f4a3d34d0861d11f
peter88213 <petert88213@gmail.com>
2025-12-18 12:43:04 +0100

    Fixed docstring.

--- src/nv_xx.py
+++ src/nv_xx.py
@@ -2,7 +2,7 @@
 
 Requires Python 3.7+
 Copyright (c) 2025 Peter Triesberger
-For further information see https://github.com/peter88213/nv_plugin
+For further information see https://github.com/peter88213/nv_xx
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 
 This program is free software: you can redistribute it and/or modify
