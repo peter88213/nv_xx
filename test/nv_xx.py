abf8a9123ce4778f98b1b88216bf30f18f46d2e0
peter88213 <petert88213@gmail.com>
2026-04-11 13:22:46 +0200

    Allowing incomplete translations.

---
+++ test/nv_xx.py
@@ -0,0 +1,41 @@
+"""xxx language package plugin for novelibre.
+
+Requires Python 3.7+
+Copyright (c) 2025 Peter Triesberger
+For further information see https://github.com/peter88213/nv_xx
+License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+import os
+import shutil
+import sys
+
+LANGUAGE_CODE = 'xx'
+TRANSLATIONS_DIR = f'{os.path.dirname(sys.argv[0])}/locale/{LANGUAGE_CODE}'
+
+
+class Plugin:
+    """Language package plugin class."""
+    VERSION = '5.42.0'
+    API_VERSION = '5.43'
+    DESCRIPTION = 'xxx language package'
+    URL = 'https://github.com/peter88213/nv_xx'
+
+    def install(self, model, view, controller):
+        if not os.path.isdir(TRANSLATIONS_DIR):
+            raise UserWarning(
+                'Translations not found:'
+                f'"{os.path.normpath(TRANSLATIONS_DIR)}".'
+            )
+
+    def uninstall(self):
+        shutil.rmtree(TRANSLATIONS_DIR, ignore_errors=True)
